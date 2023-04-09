eaa06056568ad7ac77a15c7978500512c96b5aa8
Kelsey O'Kelley <km.okelley89@gmail.com>
2023-04-09 14:40:48 -0600

    did that fix my routes?

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,72 +1,48 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// Trying async / await
-
 router.get('/', async (req, res) => {
-  try {
-    const tagData = await Tag.findAll();
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Tag.findAll()
+  .then((tags) => res.json(tags))
+  .catch((error) => res.status(500).json(error))
 });
 
 router.get('/:id', async (req, res) => {
-  try {
-    const tagData = await Tag.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [ProductTag],
-    });
-    res.status(200).json(tagData);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+  Tag.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => res.json(tag))
+  .catch((error) => res.status(400).json(error))
 });
 
 router.post('/', async (req, res) => {
-  try {
-    const tagData = await tagData.create(req.body);
-    res.status(200).json(tagData);
-  } catch (error) {
-    res.status(400).json(error);
-  }
+  Tag.create(req.body)
+  .then((tag) => res.status(200).json(tag))
+  .catch((error) => res.status(400).json(error))
 });
 
 router.put('/:id', async (req, res) => {
-  try {
-    const tagData = await tagData.findByPk({
-      where: { id: req.params.id }
-    });
-    if (!tagData) {
-      res.status(404).json({ message: 'No tag with this id!' });
-      return;
-    }
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((error) => res.status(400).json(error))
 });
+
 
 router.delete('/:id', async (req, res) => {
-  try {
-    const tagData = await tag.destroy({
-      where: {
-        id: req.params.id
-      }
-    });
-
-    if (!tagData) {
-      res.status(404).json({ message: 'No tag found with this id!' });
-      return;
-    }
-
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tag) => res.status(200).json(tag))
+  .catch((error) => res.status(400).json(error))
 });
+
 
 module.exports = router;
